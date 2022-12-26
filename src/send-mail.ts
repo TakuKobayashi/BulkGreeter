b2db67f1e592f287936ab55050ae3a4b3ef9d89e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-12-26 14:53:05 +0900

    GASからメールを送る処理を試してみた

--- src/send-mail.ts
+++ src/send-mail.ts
@@ -1,4 +1,15 @@
 
-function hello() {
+function bulkSendMail() {
+  sendMail("keep_slimbody@yahoo.co.jp")
   Logger.log("hello world");
+}
+
+function sendMail(address: string) {
+  const subject = '~~件名~~'; // メールの件名
+  const options = { };
+
+  let body = "メール本文";
+
+  GmailApp.sendEmail(address, subject, body, options);
+
 }
